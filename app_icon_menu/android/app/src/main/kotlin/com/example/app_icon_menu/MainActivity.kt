ee864c9f5b4c5b33dfbe82c44c4222ca10634c7f
YunjooCho <rollcake0317@naver.com>
2022-08-24 00:18:37 +0900

    2022/08/23 update

--- app_icon_menu/android/app/src/main/kotlin/com/example/app_icon_menu/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.app_icon_menu
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- stardup_namer/android/app/src/main/kotlin/com/example/stardup_namer/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.stardup_namer
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
